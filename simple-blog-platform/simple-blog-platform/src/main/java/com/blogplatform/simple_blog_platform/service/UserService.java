15e890d9423ed6828523a9409fa59c5548626d8d
Csushobhit <csushobhit5515@gmail.com>
2026-03-21 12:44:55 +0530

    Backend Working Properly; UI/UX Polish remaining

--- simple-blog-platform/simple-blog-platform/src/main/java/com/blogplatform/simple_blog_platform/service/UserService.java
+++ simple-blog-platform/simple-blog-platform/src/main/java/com/blogplatform/simple_blog_platform/service/UserService.java
@@ -24,6 +24,7 @@ public class UserService {
         }
 		
 		String hashedPassword = passwordEncoder.encode(user.getPassword());
+		user.setPassword(hashedPassword);
 		user.setRole("USER");
 		return userRepository.save(user);
 	}
